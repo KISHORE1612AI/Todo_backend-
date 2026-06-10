353ad766828d69760085e54f56553b278de14605
KISHORE1612AI <kishorekumarsskh@gmail.com>
2026-06-10 10:54:34 +0530

    fix: allow all origins in CORS for production

--- app.js
+++ app.js
@@ -5,11 +5,9 @@ import todoRoutes from './app/modules/todo/routes/todoRoutes.js';
 const app = express();
 
 app.use(cors({
-  origin: [
-    'http://localhost:3000',
-    'https://todo-backend-omega-three.vercel.app'
-  ],
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
 app.use(express.json());
